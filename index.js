f2f3f3f115afbdeafdf71afd89c13039adefef35
sebast825 <sebamolina825@gmail.com>
2021-08-31 17:00:36 -0300

    calcu random

--- index.js
+++ index.js
@@ -6,96 +6,188 @@ const btnNum = document.querySelectorAll('.num');
 const divResultado = document.querySelector('.resultado');
 const del = document.querySelector('.del');
 
-var valorAnterior = 0;
+const calcuAtr = document.querySelector('.calcuAtr');
+var valorAnterior = [];
 var digitador = '';
 
+function calculadoraAtr(){
+    if (atrValor==true){
+        atrValor=false;
+    }else{
+        atrValor=true;
+    };
+};
 
+var atrValor = true;
 //para los operadores
 btn.forEach((btn)=>{
-    btn.addEventListener('click',()=>{        
-       operacion(btn.innerHTML); 
+    btn.addEventListener('click',()=>{    
+        if (atrValor==true){
+            operacion(btn.innerHTML); 
+            console.log("seleccion de boton",valorAnterior ,digitador); 
+        }else{
+            numeroRandomDigitadorAtr();
+        }
+      
     });
 });   
 
 //para los numeros
 btnNum.forEach(btn=>{
     btn.addEventListener('click',()=>{
-        input.value+=btn.innerHTML;  
-        console.log(valorAnterior ,digitador)      
+        if (atrValor==true){
+            input.value+=btn.innerHTML;  
+            console.log(btn.innerHTML);
+           
+        }else{
+            numeroRandomAtr();
+        };
+          
     });
 });
 
 
 
 //devuelve el resultado dependiendo el operador
-function resultado(){
-    
-    switch(digitador){
-        
+function resultado(){    
+    switch(digitador){        
         case '+': 
             x = Number(valorAnterior) + Number(input.value);   
-            console.log("desde resultado"+digitador,valorAnterior)         
-            devolverResultado()
-            break
+            console.log("desde resultado"+digitador,valorAnterior);         
+            devolverResultado(x);
+            break;
             
         case '-':
             x = Number(valorAnterior) - Number(input.value);
-            devolverResultado()
-            break
+            devolverResultado(x);
+            break;
 
         case '*':
             x = Number(valorAnterior) * Number(input.value);
-            devolverResultado()
-            break
+            devolverResultado(x);
+            break;
 
         case '/':
             x = Number(valorAnterior) / Number(input.value);
-            devolverResultado()
-            break           
+            devolverResultado(x);
+            break;          
         default: 
-            break
+        console.log('es el default')
     };
 };
 
 
-//guarda el valor actual al poner un operador
-function guardarValor(){
-    valorAnterior = input.value;
-    divResultado.innerHTML = valorAnterior;
-    input.value="";
-    
-}
+
 //si es la primer operacion solo guarda el valor, si no lo resuelve y se prepara para la proxima operacion
 function operacion(valor){
     
-    if (digitador==''){
+    if (digitador!=''||input.innerHTML!=""){
+        console.log("operacion if"+valor,valorAnterior);
+        resultado();
+        digitador = valor;
+    }
+    else if (digitador==''){
+        
         digitador = valor;                  
-        guardarValor()
-        console.log('operacion if', digitador , valorAnterior)     
-    }else{        
-        resultado()
-        digitador = valor
-        console.log("operacion else"+valor,valorAnterior)
+        guardarValor();
+        console.log('operacion if', digitador , valorAnterior); 
+    }else{       
+        digitador = valor;
+        console.log("operacion else"+valor,valorAnterior);
     };
   
 };
+
+//para el numero random
+const numeroRandomAtr = ()=>{
+    console.log('entro numrandom')
+        g=Math.floor(Math.random()*10)+0;      
+      
+            input.value+=g;
+            console.log('numero random', g);
+    };
+
+
+// console.log(nums)
+
+var numRandomDivisor = []
+//para el divisor random
+const numeroRandomDigitadorAtr = ()=>{
+    console.log('entro digitador random');
+    h=Math.floor(Math.random()*4)+0;  
+        numRandomDivisor[0]=(h);
+    if (digitador==''){
+        (console.log('no hay digitador'))
+        devolverDigitador(h);
+        guardarValor();
+    } else{
+        console.log('hay digitador')
+        resultado();
+        devolverDigitador(h);
+
+    };
+    
+    
+};
+
+
+
+
+const devolverDigitador =numran=>{
+    
+    switch (numran){
+        case 0:  digitador = '+' ;            
+            console.log("devolverDigitador",digitador, numran);
+            break;
+        case 1:  digitador = '-';       
+        console.log("devolverDigitador",digitador, numran);
+        break;
+        case 2 :  digitador = '*';
+        console.log("devolverDigitador",digitador, numran);
+        break;
+        default:  digitador = '/';
+        console.log("devolverDigitador",digitador, numran);   
+        break;
+        }
+    };
+
+
+
+// console.log(atr)
+
+//guarda el valor actual al poner un operador
+function guardarValor(){    
+    if (input.value!=""){
+        valorAnterior = input.value;
+        divResultado.innerHTML = valorAnterior;
+        input.value="";
+    }else{
+        valorAnterior = valorAnterior;
+    };
+    
+}
+
 //desde la funcion resultado, pasa el valor actual al div, lo guarda en valor anteriory limpia el input
-function devolverResultado(){
+
+function devolverResultado(x){
     valorAnterior = x;
-    divResultado.innerHTML = (x);
+    divResultado.innerHTML = x;
     input.value = '';
 };
 
 function igualFunc(){
+    console.log('valor input: ',input.value , 'valor anterior:', valorAnterior[0], 'digiador', digitador);
+         
     resultado();
-    valorAnterior="";
+    console.log('valor input: ',input.value , 'valor anterior:', valorAnterior, 'digiador', digitador);
 };
 
 function delFunc(){
-    input.value=''
-   
+    input.value='';
 }
 
-del.addEventListener('click',()=>delFunc())
+
+del.addEventListener('click',()=>delFunc());
 igual.addEventListener('click',()=>igualFunc());
+calcuAtr.addEventListener('click',()=>calculadoraAtr());
 
